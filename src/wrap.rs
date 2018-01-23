46195229079d2c629c62d61d4808c669ebd99414
Niek Sanders <niek.sanders@gmail.com>
2018-01-23 14:52:09 -0800

    Module overview docs for wrap.

--- src/wrap.rs
+++ src/wrap.rs
@@ -1,14 +1,47 @@
-/// Trait evaluating: f(x) with x in R^1.
+//! This module provides three simple traits
+//!
+//! * RealFnEval
+//! * RealDfEval
+//! * RealD2fEval
+//!
+//! indicating that a functor computes f(x), df(x), and d2f(x) respectively.
+//! The rootfind library uses these type bounds to help implement generic
+//! driver routines.
+//!
+//! Users generally do not have to care about these traits.  Instead, generic
+//! structs adapt one or more supplied `Fn(64) -> f64` arguments:
+//!
+//! * RealFn - f(x)
+//! * RealFnAndFirst - f(x) and df(x)
+//! * RealFnAndFirstSecond - f(x), df(x), and d2f(x)
+//!
+//! Just invoke the appropriate generic struct and you're ready to go:
+//!
+//! ```
+//! use rootfind::wrap;
+//!
+//! let in_f = |x: f64| x.sin();
+//! let in_df = |x: f64| x.cos();
+//!
+//! let f = wrap::RealFnAndFirst::new(&in_f, &in_df);
+//!
+//! // f can now be used in bisection or Newton-Raphson
+//! ```
+//!
+//! The example's wrapped `f` won't compile with Halley's method because that
+//! requires the second derivative.  We would need RealFnAndFirstSecond instead.
+
+/// Trait evaluating: f(x) with x in R<sup>1</sup>.
 pub trait RealFnEval {
     fn eval_f(&self, x: f64) -> f64;
 }
 
-/// Trait evaluating the derivative: df(x) with x in R^1.
+/// Trait evaluating the derivative: df(x) with x in R<sup>1</sup>.
 pub trait RealDfEval {
     fn eval_df(&self, x: f64) -> f64;
 }
 
-/// Trait evaluating the second derivative: d2f(x) with x in R^1.
+/// Trait evaluating the second derivative: d2f(x) with x in R<sup>1</sup>.
 pub trait RealD2fEval {
     fn eval_d2f(&self, x: f64) -> f64;
 }
